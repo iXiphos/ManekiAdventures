2b6580e5a9c2e7bbfea70be5d2e845c51d330b63
Aidan Murphy <aidan.murphy@mymail.champlain.edu>
2020-07-09 12:55:54 -0400

    Fixing Up potion crafting, currently a known issue where it has to be added in a certain order in order for it to craft properly. Tossing potions now works properly

--- ManekiAdventures/Assets/Scripts.shef/Inventory/InventorySlot.cs
+++ ManekiAdventures/Assets/Scripts.shef/Inventory/InventorySlot.cs
@@ -40,13 +40,14 @@ public class InventorySlot : MonoBehaviour
 
             if (item.Attribute != attribute.empty && item.Discriptor != discriptor.empty)
             {
+                Debug.LogError("Yikes");
                 GameObject.Find("Player").GetComponent<tossPotion>().createPotion(item);
+                StartCoroutine(GameObject.Find("Player").GetComponent<tossPotion>().createTarget());
                 Inventory.instance.Remove(item);
                 ClearSlot();
             }
             else
             {
-                Debug.LogError("Adding stt");
                 if (crafting.GetComponent<PotionCreation>().piece1 == null)
                 {
                     crafting.GetComponent<PotionCreation>().piece1 = item;
