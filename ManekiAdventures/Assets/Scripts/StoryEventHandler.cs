55d2b7e6d2b1b82fd7c02f594e2c5fce4581078f
Michelle Nie <michelle.nie@mymail.champlain.edu>
2020-07-28 13:58:54 -0400

    Adding pickup unique interactions. Also adding tracking for unique interactions so they only get played once.

--- ManekiAdventures/Assets/Scripts/StoryEventHandler.cs
+++ ManekiAdventures/Assets/Scripts/StoryEventHandler.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class StoryEventHandler : MonoBehaviour
 {
     DialogueEventController dialogueEventController;
+    public Dictionary<string, bool> uniqueEventTracker; //string name of event, bool hasInteracted? (true if yes)
+
     // Start is called before the first frame update
     void Start()
     {
+        uniqueEventTracker = new Dictionary<string, bool>();
         dialogueEventController = GameObject.Find("DialogueEventController").GetComponent<DialogueEventController>();
 
         //StartFirstDialogue();
@@ -35,16 +38,32 @@ public class StoryEventHandler : MonoBehaviour
 
     public void PickedUpEvent(string itemName)
     {
-        dialogueEventController = GameObject.Find("DialogueEventController").GetComponent<DialogueEventController>();
+        string eventName = "";
+        switch(itemName)
+        {
+            case "Decrease":
+            case "Increase":
+                eventName = "UNIQUE_PICKUP_INCREASEDECREASE";
+                break;
+            case "Size":
+                eventName = "UNIQUE_PICKUP_SIZE";
+                break;
+            case "Weight":
+                eventName = "UNIQUE_PICKUP_WEIGHT";
+                break;
+            case "Friction":
+                eventName = "UNIQUE_PICKUP_FRICTION";
+                break;
+        }
 
-        if (itemName == "Decrease" || itemName == "Increases")
-        {
-            dialogueEventController.ExecuteEvent("UNIQUE_PICKUP_INCREASEDECREASE");
-        }
-        if (itemName == "Size")
-        {
-            dialogueEventController.ExecuteEvent("UNIQUE_PICKUP_SIZE");
-        }
+        if (uniqueEventTracker[eventName]) // if the player has seen this dialogue, do not execute
+            return;
+        else
+            uniqueEventTracker[eventName] = true; // keep track of what you've seen
+
+
+        dialogueEventController = GameObject.Find("DialogueEventController").GetComponent<DialogueEventController>();
+        dialogueEventController.ExecuteEvent(eventName);
     }
 
     public void ExitApplication()
